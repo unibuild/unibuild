af3ba19114d816fa7fc6fdd0343eecb53a5d3a2d
andortoth <andor.toth@ewise.hu>
2019-02-18 11:32:32 +0100

    build script fix

--- build/scripts/linux/build-server.sh
+++ build/scripts/linux/build-server.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 # ----------------------------------------------------------------------------
 cd /opt/unibuild/mojo-unix
-mvn -U clean install -DskipTests=true
+mvn -U clean install -DskipTests=true | exit 1
 
 cd /opt/unibuild/unibuild-parent
-mvn -U clean install
+mvn -U clean install | exit 1
 
 cd /opt/unibuild/unibuild-plugins/unibuild-plugins-parent
-mvn -U clean install
+mvn -U clean install | exit 1
 
 cd /opt/unibuild/unibuild-server-common
-mvn -U clean install
+mvn -U clean install | exit 1
 
 cd /opt/unibuild/unibuild-server
-mvn -U clean install
+mvn -U clean install | exit 1
 
 cd /opt/unibuild/unibuild-web-setup
-mvn -U clean install
+mvn -U clean install | exit 1
 
 cd /opt/unibuild/unibuild-server-installer-linux
-mvn -U clean install
+mvn -U clean install | exit 1
 
 
