79bd7cc5d1401ceb69b228ad5f58b5747afc4ea9
andortoth <andor.toth@ewise.hu>
2019-02-18 12:40:19 +0100

    linux server build script fix

--- build/scripts/linux/build-server.sh
+++ build/scripts/linux/build-server.sh
@@ -3,23 +3,30 @@
 set -e
 cd ./mojo-unix
 mvn -U clean install -DskipTests=true
+cd ..
 
 cd ./unibuild-parent
 mvn -U clean install
+cd ..
 
 cd ./unibuild-plugins/unibuild-plugins-parent
 mvn -U clean install
+cd ../..
 
 cd ./unibuild-server-common
 mvn -U clean install
+cd ..
 
 cd ./unibuild-server
 mvn -U clean install
+cd ..
 
 cd ./unibuild-web-setup
 mvn -U clean install
+cd ..
 
 cd ./unibuild-server-installer-linux
 mvn -U clean install
+cd ..
 
 
